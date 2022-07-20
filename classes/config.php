597b0224447a85d6beac603ed0f2a53c32ef51c3
hosszugyula <hosszu87@gmail.com>
2022-07-20 20:28:34 +0200

    Create a config file do Database Connection

---
+++ classes/config.php
@@ -0,0 +1,7 @@
+<?php
+	const CONFIG_RESPONSE_TYPE = "json"; //if u want to use xml change the CONFIG_RESPONSE_TYPE to xml
+	const CONFIG_SERVER_NAME = "localhost";
+	const CONFIG_DB_NAME = "id17635720_secret_server";
+	const CONFIG_DB_USER = "id17635720_secretuser";
+	const CONFIG_DB_PW = "Q+MnRDIRox+Z[I8}";
+?>
